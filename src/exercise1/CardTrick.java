e53ba3e3b8cea8fdc11183d83dc53936b8f906dc
Kianoush Heidari <heidarik@sheridancollege.ca>
2023-06-01 22:30:19 -0400

    I'm done!

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
  *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * 
+ * @author Kianoush Heidari June 2023
  */
 public class CardTrick {
     
